1583beed12e41c4fe2dae537e08ab40f037b2fad
Andrés Cifuentes <andrescifuentesdorado@gmail.com>
2024-12-02 16:53:31 -0500

    End section 11
    CRUD Hospital

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+
 const { response } = require("express");
 const { v4: uuidv4 } = require('uuid');
 const { actualizarImagen } = require("../helpers/actualizar-imagen");
@@ -69,6 +72,24 @@ const fileUpload = ( req, res = response ) => {
 
 }
 
+const ImageReturn = (req, res = response) => {
+
+    const tipo = req.params.tipo;
+    const foto = req.params.foto;
+
+    const pathImg = path.join( __dirname, `../uploads/${ tipo }/${ foto }` );
+
+    //Imagen por defecto
+    if ( fs.existsSync( pathImg ) ){
+        res.sendFile( pathImg );
+    } else {
+        const pathImg = path.join( __dirname, `../uploads/No_Image_Available.jpg` );
+        res.sendFile( pathImg );
+    }
+
+}
+
 module.exports = {
-    fileUpload
+    fileUpload,
+    ImageReturn
 }
--- routes/uploads.js
+++ routes/uploads.js
@@ -8,13 +8,14 @@ const { Router } = require('express');
 const expressfileUpload = require('express-fileupload');
 const { validarJWT } = require('../middlewares/validar-JWT')
 const { getTodo, getDocumentosColeccion } = require('../controllers/busquedas');
-const { fileUpload } = require('../controllers/uploads');
+const { fileUpload, ImageReturn } = require('../controllers/uploads');
 
 const router = Router();
 
 router.use(expressfileUpload());
 
 router.put('/:tipo/:id', validarJWT, fileUpload);
+router.get('/:tipo/:foto', ImageReturn );
 
 
 module.exports = router;
